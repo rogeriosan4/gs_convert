450532bd056e38a9cea60d2e7f30da397714982d
rogeriosan4 <71185048+rogeriosan4@users.noreply.github.com>
2020-11-20 19:19:52 -0300

    atualização da descrição sobre PORTABLE_ENGINE
    descrição melhor sobre a utilidade de 'gs.PORTABLE_ENGINE'

--- example_convert_pdf.py
+++ example_convert_pdf.py
@@ -2,9 +2,10 @@ import gs_convert as gs
 
 
 if __name__ == "__main__":
-
-    #gs_portable_path_example = 'C:/Users/<username>/Downloads/CommonFiles/Ghostscript/bin/gswin64c'
-    #gs.PORTABLE_ENGINE = gs_portable_path_example 
+    
+    ##optional call to portable library, if you don't have ghostscript recognized in environment variables
+    ##gs_portable_path_example = 'C:/Users/<username>/Downloads/CommonFiles/Ghostscript/bin/gswin64c'
+    ##gs.PORTABLE_ENGINE = gs_portable_path_example 
     
     #declaring output file: convert all pages to a one file
     #gs.pdf2txt('files/file.pdf', 'files/file.txt')
